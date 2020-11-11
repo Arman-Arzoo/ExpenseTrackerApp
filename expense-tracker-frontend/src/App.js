855ff7373ad30dea26bcba72c1ad5497e20e4cc7
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-11 22:44:52 +0500

    Add all component of Expense Tracker App

--- expense-tracker-frontend/src/App.js
+++ expense-tracker-frontend/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-
+import {Header} from "./components/Header";
+import {Balance} from "./components/Balance";
+import {IncomeExpenses} from "./components/IncomeExpenses";
+import {TransactionList} from "./components/TransactionList";
+import {AddTransaction} from "./components/AddTransaction";
 
 function App() {
-
-  
-  return (
-    
-    <div className="main-container">
-     
-        
-        
+  return ( 
+    <div>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+       
+      </div>  
     </div>
   );
 }
-
 export default App;
