5f59c7806f902aca1a1a63599242e55c9ffe5482
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-12 15:02:07 +0500

    update baclace.js and incomeExpense.js and remove test file arman.js

--- expense-tracker-frontend/src/components/IncomeExpenses.js
+++ expense-tracker-frontend/src/components/IncomeExpenses.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amonut = transactions.map((transaction) => transaction.amount);
+  const income = amonut
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense =
+    amonut.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -(1).toFixed(2);
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">$0.00</p>
+        <p className="money plus">${income}</p>
       </div>
-      
+
       <div>
         <h4>Expense</h4>
-        <p className="money minus">$0.00</p>
+        <p className="money minus">${expense}</p>
       </div>
     </div>
   );
