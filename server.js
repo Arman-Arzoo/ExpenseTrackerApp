2fe810f34759ece65028f5d75e5c831f7a5f1b02
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-14 14:59:57 +0500

    change development to production and update server.js
    NOw our application is completed and successfully  run in production mode.now this is our last commit of this application

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
@@ -13,10 +14,18 @@ const transRouter = require('./routes/transRouter')
 const app = express();
 
 app.use(express.json());
-app.use('/api/v1/transactions',transRouter);
 
 if(process.env.NODE_ENV == "development"){
     app.use(morgan("dev"));
+}
+
+app.use('/api/v1/transactions',transRouter);
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static('expense-tracker-frontend/build'));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'expense-tracker-frontend','build','index.html'));
+    });
 }
 
 
